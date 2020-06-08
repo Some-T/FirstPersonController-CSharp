df8bb849a9645ca4c090ef408bf583b798c53a6e
Jamie Cropley <somet@protonmail.com>
2020-06-08 21:40:06 +0100

    50 % completion of code for First person controller

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+// Below code is based off: https://youtu.be/Ohcx16kfuaA
+
 
 public class FirstPersonController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(0f, 100f )]
+    public float mouseSensitivity = 70f;
+
+    float horizontal;
+    float vertical;
+    float mouseX;
+    float mouseY;
+
+    Quaternion deltaRotation;
+    Vector3 deltaPosition;
+
+    Rigidbody rbody;
+
     void Start()
     {
-        
+        rbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetInputs();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
+
+    private void FixedUpdate()
+    {
+        deltaRotation = Quaternion.Euler(Vector3.up * mouseX * mouseSensitivity * Time.fixedDeltaTime);
+        rbody.MoveRotation(rbody.rotation * deltaRotation);
+    }
+
+
+    void GetInputs()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+
 }
