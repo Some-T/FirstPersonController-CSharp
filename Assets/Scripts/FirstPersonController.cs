75b9af56c4c20452eae9c523c10c231fb2e798e5
Jamie Cropley <somet@protonmail.com>
2020-06-10 19:13:01 +0100

    Initial movement code applied without mouse movement

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    public float speed = 5;
+    public float jumpPower = 4;
+
+    Rigidbody rb;
+    CapsuleCollider col;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        float Horizontal = Input.GetAxis("Horizontal") * speed;
+        float Vertical = Input.GetAxis("Vertical") * speed;
+        Horizontal *= Time.deltaTime;
+        Vertical *= Time.deltaTime;
+        transform.Translate(Horizontal, 0, Vertical);
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
     }
 }
