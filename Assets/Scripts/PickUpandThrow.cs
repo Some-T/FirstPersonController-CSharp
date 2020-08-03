82e808471e4fd5a67f63fe5fc21a8d38d705c1d0
Jamie Cropley <somet@protonmail.com>
2020-08-03 20:58:56 +0100

    Added new script for picking up and throwing objects in game. Not currently completed though, is only the start of the script. Lighting also changed I think relative to Unity 2020 upgrade?

---
+++ Assets/Scripts/PickUpandThrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://docs.unity3d.com/ScriptReference/Physics.OverlapSphere.html
+
+
+public class PickUpandThrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
