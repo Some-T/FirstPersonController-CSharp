f23cd3de8cbc3c01c3a231176b321a0e90c2e2a9
Jamie Cropley <somet@protonmail.com>
2020-08-13 17:27:57 +0100

    Added a link to code.

--- Assets/Scripts/PickUpandThrow.cs
+++ Assets/Scripts/PickUpandThrow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // https://docs.unity3d.com/ScriptReference/Physics.OverlapSphere.html
-
+// https://forum.unity.com/threads/overlap-sphere-and-the-best-way-to-pick-up-objects.946387/
 
 public class PickUpandThrow : MonoBehaviour
 {
